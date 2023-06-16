a735a4afcc52055827f5b00b4c218049c0fc2d81
Lucca Gabriel Vital Cristino <lucca.gabriel@faveni.intranet>
2023-06-16 17:55:03 -0300

    inclusão do menu lateral

---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,42 @@
+import { Drawer, useTheme, Avatar, Divider, List, ListItemButton, ListItemText, Icon,  ListItemIcon } from "@mui/material"
+import React from "react";
+import { Box } from '@mui/system';
+
+interface IMenuLateralProps {
+    children: React.ReactNode;
+}
+
+export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
+    const theme = useTheme();
+    
+    return (
+        <>
+            <Drawer open={true} variant='permanent'>
+                <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+                    <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+                        <Avatar src="" sx={{ height: theme.spacing(12), width: theme.spacing(12)}}/>
+                    </Box>
+
+                    <Divider />
+
+                    <Box flex={1}>
+                        <List component="nav">
+                            <ListItemButton>
+
+                                <ListItemIcon>
+                                    <Icon>home</Icon>
+                                </ListItemIcon>
+
+                                <ListItemText primary="Página inicial" />
+                            </ListItemButton>
+                        </List>
+                    </Box>
+                </Box>
+            </Drawer>
+
+            <Box height='100vh' marginLeft={theme.spacing(28)}>
+                {children}
+            </Box>
+        </>
+    );
+};
